9a20c415fff2a5dd62386c8ba513f3a1f9fb5ff6
hugoewarrior <hugoewarrior@gmail.com>
2020-02-05 21:34:06 +0000

    Se agrega UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,206 @@
 import React, { useCallback, useState, useEffect } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { ExcelFile } from './manageExcel/excelList'
+import {
+  Grid, makeStyles, createStyles, Theme,
+  ButtonGroup, Button, ListItem, ListItemIcon,
+  ListItemText, Typography, Divider, IconButton,
+  Tooltip, Backdrop, CircularProgress, Snackbar, Slide
+} from '@material-ui/core'
+import { GenericAppBar } from './_utils/appbar'
+import { NoteAdd, FileCopy, Clear } from '@material-ui/icons'
+import { TransitionProps } from '@material-ui/core/transitions';
+
+const SlideTransition = (props: TransitionProps) => <Slide {...props} direction="up" />
+
 
 export const App = (props: any) => {
 
   const [files, setFiles] = useState([])
+  const [working, setWorking] = useState(false)
+  const [snackMessage, setMessage] = useState('' as string)
+  const [results, setResults] = useState([])
   const excel = new ExcelFile()
 
 
-  useEffect(() => console.log(excel.working), [excel.working])
+  useEffect(() => console.log(working), [working])
 
 
   const onDrop = useCallback(acceptedFiles => {
-    setFiles(acceptedFiles)
+    setFiles(acceptedFiles);
   }, [])
 
   const loadFiles = async () => {
-    excel.readmultifiles(files).then((e: any) => excel.normalizeFiles(e))
+    setWorking(true);
+    excel.readmultifiles(files)
+      .then((e: any) => {
+        excel.normalizeFiles(e).then((e: any) => {
+          if (e.length > 0) {
+            setMessage(`${e.length} results found`);
+            setResults(e)
+          }
+          else {
+            setMessage(`We re sorry, no results were found`);
+            setResults([])
+          }
+        })
+      })
+      .finally(() => {
+        setWorking(false);
+      })
+  }
+
+  const exportFile = async () => {
+    setWorking(true)
+    excel.generateExcelFile(results).then((r) => {
+      r ? setMessage("The file was generated succesfuly") :
+        setMessage("Error: please try again")
+    }).finally(() => setWorking(false))
+  }
+
+  const deltedSelected = (row: any) => {
+    /* let f = files.filter((r: any) => r.name =! row.name)
+    setFiles(f) */
+    console.log(files)
+
   }
 
   const deleteFiles = () => {
     setFiles([])
+    setResults([])
   }
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const classes = mainAppStyles();
 
   return (
-    <div>
-      <div
-        style={
-          {
-            minHeight: 500,
-            display: 'flex',
-            flex: 3,
-            flexDirection: 'column',
-            backgroundColor: '#24292e'
-          }
-        }
-        {...getRootProps()}>
-        <input {...getInputProps()} />
-        <div
-          style={{
-            textAlign: 'center',
-            padding: '5%'
-          }}
-        >
-          {
-            isDragActive ?
-
-              <p style={{ color: 'white' }}>Drop the files here ...</p> :
-              <p style={{ color: 'white' }}>Drag 'n' drop some files here, or click to select files</p>
-          }
-        </div>
-        <div>
-          {files.length > 0 ? files.map((f: any, key: number) =>
-            <div
-              key={key}
-              style={{
-                display: 'flex',
-                flex: 1,
-                flexDirection: 'row',
-                padding: '3%', color: 'white'
-              }} >
-              <div style={{
-                flex: 3, float: 'left'
-              }}>
-                {f.name}
-              </div>
-              <div style={{
-                flex: 1, float: 'right'
-              }}>
-                {key}
-              </div>
-            </div>
-          ) :
-            <div style={{
-              flex: 1, textAlign: 'center',
-              padding: '10%', color: 'red'
+    <Grid
+      container
+    >
+      <GenericAppBar title={"DataSur A.I."} />
+      <Grid item
+        xs={12} sm={12} md={12} lg={12} xl={12}
+        className={classes.itemRoot}>
+        {files.length < 1 ?
+          <div
+            className={classes.bucket}
+            style={{
+              borderColor: isDragActive ? "#2196f3" : "#ACB3B3",
             }}
-            >NO files added yet </div>}
-        </div>
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            <div
+              className={classes.centerAwaiter}
+            >
+              <NoteAdd color={isDragActive ? "primary" : "disabled"} fontSize="large" />
+              <p> Drop the files here or Click to search ...</p>
+            </div>
+          </div> :
+          <div className={classes.fileList}>
 
-      </div>
+            <Typography variant="h4"> {`${files.length} elementos seleccionados:`} </Typography>
+            <Divider style={{
+              marginTop: 10, marginBottom: 10
+            }} />
+            {files.map((row: any, k: number) =>
+              <ListItem key={k}>
+                <ListItemIcon>
+                  <FileCopy />
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.listItemText }}
+                  primary={row.name} />
+                <Tooltip title="Borrar" placement="left">
+                  <IconButton onClick={() => deltedSelected(row)}>
+                    <Clear />
+                  </IconButton>
+                </Tooltip>
+              </ListItem>)}
+          </div>}
 
-      <button disabled={files.length < 1} onClick={loadFiles} >
-        Process Files
-      </button>
 
-      <button disabled={files.length < 1} onClick={deleteFiles} >
-        Clear Files
-      </button>
+      </Grid>
+      <Grid item
+        xs={12} sm={12} md={12} lg={12} xl={12}
+        className={classes.buttons}
+      >
+        <ButtonGroup size="medium">
+          <Button variant="contained" color="primary" disabled={files.length < 1} onClick={loadFiles}>Process Files</Button>
+          <Button disabled={results.length < 1}
+            variant="contained" color="primary"
+            onClick={exportFile}>
+            Download Results
+          </Button>
+          <Button disabled={files.length < 1} onClick={deleteFiles}>Clear Files</Button>
 
-      {files.length > 0 ?
-        <button onClick={()=>excel.generateExcelFile(excel.currentF)} >
-          Download Results
-      </button> : null}
-    </div>
+        </ButtonGroup>
+      </Grid>
+      <Backdrop className={classes.backdrop} open={working}>
+        <Typography variant="caption"> Processing Data, please wait... </Typography>
+        <CircularProgress style={{
+          textAlign: "center", marginLeft: 10
+        }} color="inherit" />
+      </Backdrop>
+      <Snackbar
+        message={snackMessage}
+        open={Boolean(snackMessage)}
+        autoHideDuration={10000}
+        TransitionComponent={SlideTransition}
+        action={
+          <IconButton color="inherit" onClick={() => setMessage('')}>
+            <Clear />
+          </IconButton>
+        }
+      />
+    </Grid>
 
   )
 }
+
+const mainAppStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    itemRoot: {
+      display: "flex",
+      flexDirection: "column",
+      flex: 1,
+    },
+    bucket: {
+      textAlign: "center",
+      margin: 20,
+      padding: 20,
+      borderStyle: "dashed",
+      borderColor: "#ACB3B3",
+      height: "70vh",
+      cursor: "pointer"
+    },
+    fileList: {
+      margin: 20,
+      padding: 20,
+      height: "65vh",
+    },
+    centerAwaiter: {
+      left: "50%",
+      top: "50%",
+      position: "absolute",
+      transform: "translate(-50%, -50%)",
+      textAlign: 'center'
+    },
+    buttons: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    listItemText: {
+      fontSize: '0.9em'
+    },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+
+
+  }),
+);
--- src/manageExcel/excelList.ts
+++ src/manageExcel/excelList.ts
@@ -1,6 +1,7 @@
 import * as XLSX from 'xlsx';
 import { bicgVoc, itcgVoc, senOwner, BICGLimit } from '../_constants/vocabularies'
 import _ from "lodash";
+import { resolve } from 'dns';
 
 
 
@@ -8,7 +9,7 @@ export class ExcelFile {
 
     reader: FileReader
     working: boolean = false
-    currentF: any[]
+    public currentF: any[]
 
 
     constructor() {
@@ -21,20 +22,20 @@ export class ExcelFile {
         return new Promise((resolve) => {
             let resp: any = []
             for (const [i, file] of files.entries() as any) {
-                const reader = new FileReader()
-                reader.onabort = () => console.log('file reading was aborted')
-                reader.onerror = () => console.log('file reading has failed')
+                const reader = new FileReader();
+                reader.onabort = () => console.log('file reading was aborted');
+                reader.onerror = () => console.log('file reading has failed');
                 reader.onload = (evt: any) => {
                     const bstr = evt.target.result;
                     const wb = XLSX.read(bstr, { type: 'binary' });
                     const wsname = wb.SheetNames[0];
                     const ws = wb.Sheets[wsname];
                     const data = XLSX.utils.sheet_to_json(ws);
-                    resp[i] = data
-                    if (i + 1 === files.length) resolve(resp)
+                    resp[i] = data;
+                    if (i + 1 === files.length) resolve(resp);
 
                 };
-                reader.readAsBinaryString(file)
+                reader.readAsBinaryString(file);
             }
         }
         )
@@ -43,44 +44,54 @@ export class ExcelFile {
 
     normalizeFiles(files: any[]) {
         console.log('Processing...', files)
+        return new Promise((resolve) => {
+            /**First filter layer */
+            let mergedData: any[] = [].concat.apply([], files);
 
-        /**First filter layer */
-        let mergedData: any[] = [].concat.apply([], files)
-
-        /**Calculate price per unit (P/U) */
-        for (const row of mergedData) {
-            row["P/U"] = (+(row["US$ FOB"] / row.CANTIDAD).toFixed(2))
-            row.DIA = parseInt(row.DIA)
-            row.MES = parseInt(row.MES)
-            row.AÑO = parseInt(row.AÑO)
-        }
-
-        /** merged Arrays */
-        this.evaluateConditions(mergedData)
+            /**Calculate price per unit (P/U) */
+            for (const row of mergedData) {
+                row["P/U"] = (+(row["US$ FOB"] / row.CANTIDAD).toFixed(2));
+                row.DIA = parseInt(row.DIA);
+                row.MES = parseInt(row.MES);
+                row.AÑO = parseInt(row.AÑO);
+            }
+            /** merged Arrays */
+            let resp: any[] = this.evaluateConditions(mergedData);
+            resolve(resp)
+            //assign val
+            //console.log(console.log(mergedData, resp, _.differenceWith(mergedData, resp, _.isEqual)));
+        })
     }
 
     evaluateConditions(mergedData: any[]) {
-        let fStage: any[] = mergedData.filter((reg) => (reg["P/U"] >= BICGLimit))
+        let fStage: any[] = mergedData.filter((reg) => (reg["P/U"] >= BICGLimit));
         let sndStage: any[] = fStage.filter((reg) => this.findStringOnSentence((reg[senOwner] as string)
-            .toLowerCase()))
-        this.currentF = _.uniqWith(sndStage, _.isEqual)
-        return false
+            .toLowerCase()));
+        return _.uniqWith(sndStage, _.isEqual);
     }
 
     findStringOnSentence(sentence: string) {
         /** for BICG */
-        return (bicgVoc.some((str) => sentence.search(str.sen) >= 0) &&
-            !(itcgVoc.some((str) => sentence.search(str.sen) >= 0)))
+        let resp = (bicgVoc.some((str) => sentence.search(str.sen) >= 0) &&
+            !(itcgVoc.some((str) => sentence.search(str.sen) >= 0)));
+        /* console.log(resp, sentence) */
+        return resp
     }
 
     generateExcelFile(importVal: Object[]) {
-        console.log(importVal)
-        let ws = XLSX.utils.json_to_sheet(importVal)
-        let wb = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, "Reporte");
-        XLSX.writeFile(wb, "sheetjs.xlsx");
-    }
+        return new Promise((resolve) => {
+            if (importVal.length > 0) {
+                let ws = XLSX.utils.json_to_sheet(importVal);
+                let wb = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(wb, ws, "Reporte");
+                XLSX.writeFile(wb, "sheetjs.xlsx");
+                resolve(true)
+            }
+            else resolve(false)
+        })
 
+
+    }
 
 }
 
